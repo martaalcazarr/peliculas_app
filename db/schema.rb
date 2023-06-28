033212dd154a20bc0fb3406bc0d35d8a176f2a61
martaalcazarr <martaalcazarredondo@gmail.com>
2023-06-27 21:46:05 -0400

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_06_27_215224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "documentary_films", force: :cascade do |t|
     t.string "name"
